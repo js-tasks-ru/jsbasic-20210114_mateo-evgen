94b9571be3f4e2be27cc62f6b40d7df4db6ff697
Mate088 <matstudy60@gmail.com>
2021-01-22 14:31:39 +0300

    Выполнил блок работ с занятия 1

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,8 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    str = str.slice(0, maxlength - 1) + '…';
+   }
+   return str;
 }
